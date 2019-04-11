3d64ace26adc0f67e7bbc6e0674d8c9a4f33fd26
Daniel Fernandes <daniel.dourado@gmail.com>
2019-04-11 18:43:16 +0000

    implementação de planejamento financeiro

--- PlanejamentoFinanceiro.gd
+++ PlanejamentoFinanceiro.gd
@@ -4,18 +4,30 @@ extends VBoxContainer
 # var a = 2
 # var b = "text"
 var slider_names = ['Saúde','Segurança','Educação','Mobilidade','Lazer']
+var max_value = 100000
+var remaining_value = 0
 var sliders = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	remaining_value = max_value
+	get_node("Label").text = "Orçamento: R$"+str(max_value)
+	set_remaining_value(remaining_value)
+	
 	create_sliders(slider_names)
 
 func create_sliders(slider_names):
 	for slider_name in slider_names:
 		var temp_slider = load("res://PlanejamentoFinanceiroSlider.tscn").instance()
-		temp_slider.change_label_name(slider_name)
-		sliders.append(temp_slider)
 		add_child(temp_slider)
+		temp_slider.change_label_name(slider_name)
+		temp_slider.set_max_value(max_value)
+		sliders.append(temp_slider)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func set_remaining_value(value):
+	get_node("Label2").text = "Orçamento restante: R$"+str(value)
+
+func _process(delta):
+	var total_value = 0
+	for slider in sliders:
+		total_value += slider.get_slider_value()
+	set_remaining_value(max_value - total_value)
--- PlanejamentoFinanceiroSlider.gd
+++ PlanejamentoFinanceiroSlider.gd
@@ -1,14 +1,19 @@
 extends VBoxContainer
 
-var label = null;
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	label = get_node("HBoxContainer/Label")
-	label.text = "daniel"
+	pass
 
 func change_label_name(labelName):
-	label.text = labelName
+	get_node("HBoxContainer/Label").text = labelName+":"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func set_max_value(value):
+	var hslider = get_node("HSlider")
+	hslider.max_value = value
+	hslider.step = value/100
+
+func _process(delta):
+	var label_text = str(get_node("HSlider").value)
+	get_node("HBoxContainer/Label2").text = label_text
+	
+func get_slider_value():
+	return get_node("HSlider").value
