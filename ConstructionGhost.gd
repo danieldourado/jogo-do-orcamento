eb69ec03935fc7231cc412f6b05aca791c48a7c0
Daniel Fernandes <daniel.dourado@gmail.com>
2019-04-16 18:37:45 +0000

    sistema pra só deixar construir se não tiver em cima de um collider

--- ConstructionGhost.gd
+++ ConstructionGhost.gd
@@ -1,17 +1,30 @@
-extends TextureButton
+extends Area2D
 
 var new_building = null;
+var texture_rect = null;
+var can_build = false;
 
 func _ready():
-	pass # Replace with function body.
+	$".".connect("area_entered", self, "_area_entered")
+	$".".connect("area_exited", self, "_area_exited")
+	$"TextureRect".connect("pressed", self, "_pressed")
+	texture_rect = get_node("TextureRect")
+	
+func _area_entered (body):
+	texture_rect.modulate = Color(1, 0, 0)
+	can_build = false
+	
+func _area_exited (body):
+	texture_rect.modulate = Color(1, 1, 1)
+	can_build = true
 	
 func _process(delta):
-	var position = get_viewport().get_mouse_position()
-	rect_position.x = position.x
-	rect_position.y = position.y
+	var _position = get_viewport().get_mouse_position()
+	$".".position = _position
 		
-			
+
 func _pressed():
+	if !can_build: return
 	new_building = load("res://Construction.tscn").instance()
 	get_node("/root").add_child(new_building)
 	var position = get_viewport().get_mouse_position()
