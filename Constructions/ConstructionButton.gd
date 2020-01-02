f8b304c72bfb536688f320c7eed452f932af122c
Daniel D. Fernandes <daniel.dourado@gmail.com>
2020-01-02 19:11:19 -0300

    clear_pendin_construction when construcion button is clicked

--- Constructions/ConstructionButton.gd
+++ Constructions/ConstructionButton.gd
@@ -9,9 +9,10 @@ func set_construction_button(construction_name, construction_texture_name):
 	get_node("BuildingName").text = construction_name
 
 func _on_Texture_pressed():
+	clear_pendin_construction()
 	var construction_ghost = load("res://Constructions/NewConstruction.tscn").instance()
 	construction_ghost.construction_texture_name = construction_texture_name
-	get_node("/root/Node2D/GameplayControl").add_child(construction_ghost)
+	get_node("/root/Node2D/GameplayControl/PlayerConstructions").add_child(construction_ghost)
 	construction_ghost.set_ghost_texture(get_node("Texture").texture_normal)
 	construction_ghost.set_name(get_name())
 
@@ -27,3 +28,8 @@ func decrease_counter():
 	var amount = int(get_node("counter/counter-label").text)-1
 	self.amount = amount
 	get_node("counter/counter-label").text = str(amount)
+	
+func clear_pendin_construction():
+	for construction in get_node("/root/Node2D/GameplayControl/PlayerConstructions").get_children():
+		if construction.built == false:
+			get_node("/root/Node2D/GameplayControl/PlayerConstructions").remove_child(construction)
